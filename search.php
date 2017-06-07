e6bc11b89ca9317618e204b0efc51be60aae912e
ben <bwhitlock@cyberbility.com>
2017-06-07 01:40:06 -0400

    scss theme variables created

--- search.php
+++ search.php
@@ -16,7 +16,7 @@ get_header(); ?>
 				if ( have_posts() ) : ?>
 					<header class="page-header">
 						<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'voyager-theme' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-					</header><!-- .page-header -->
+					</header>
 					<?php
 					while ( have_posts() ) : the_post();
 						get_template_part( 'template-parts/content', 'search' );
--- single.php
+++ single.php
@@ -16,18 +16,17 @@ get_header(); ?>
 				while ( have_posts() ) : the_post();
 					get_template_part( 'template-parts/content', get_post_format() );
 					the_post_navigation();
-					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
 						comments_template();
 					endif;
-				endwhile; // End of the loop.
+				endwhile;
 				?>
 			</setion>		
 			<aside class="col-sm-4">
 				<?php get_sidebar(); ?>
 			</aside>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main>
+	</div>
 
 <?php
 get_footer();
