2cf5305cffa2ccf4cc964909e8dbe1b26c19653d
ben <bwhitlock@cyberbility.com>
2017-06-08 15:27:51 -0400

    created footer widgets

--- footer.php
+++ footer.php
@@ -13,12 +13,39 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer container" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'voyager-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'voyager-theme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'voyager-theme' ), 'voyager-theme', '<a href="https://automattic.com/" rel="designer">Ben Whitlock</a>' ); ?>
-		</div><!-- .site-info -->
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-5 footer__logo">
+					<h1 class="footer__logo__title">Logo</h1>
+					<h5 class="footer__logo__subtitle">no tail left behind.</h5>
+				</div>
+				<div class="col-sm-7 footer__widgets">
+					<div class="row">
+						<div class="col-sm-4">
+							<?php dynamic_sidebar('footer-one'); ?>
+						</div>
+						<div class="col-sm-4">
+							<?php dynamic_sidebar('footer-two'); ?>
+						</div>
+						<div class="col-sm-4">
+							<?php dynamic_sidebar('footer-three'); ?>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-4">
+							<?php dynamic_sidebar('footer-four'); ?>
+						</div>
+						<div class="col-sm-4">
+							<?php dynamic_sidebar('footer-five'); ?>
+						</div>
+						<div class="col-sm-4">
+							<?php dynamic_sidebar('footer-six'); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,15 @@
 (function($) {
+	
+	
+	$(window).on('load', function(event) {
+	    var scrollValue = $(window).scrollTop();		
+	    if (scrollValue > 70) {
+	         $('.navbar').addClass('stuck');
+	    }
+	});
+
 	$(window).on('scroll', function(event) {
-	    var scrollValue = $(window).scrollTop();
+		var scrollValue = $(window).scrollTop();
 	    if (scrollValue > 70) {
 	         $('.navbar').addClass('stuck');
 	    } else if (scrollValue < 70) {
