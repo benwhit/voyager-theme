7c5c3b68d6291fef04d0c7c0a8fba25c161218f0
ben <bwhitlock@cyberbility.com>
2017-06-15 01:48:36 -0400

    created adopt single post template

--- front-page.php
+++ front-page.php
@@ -22,73 +22,105 @@ get_header();
 			if( have_rows('flexible_content') ):
 				while ( have_rows('flexible_content') ) : the_row();
 
+				// Category Section Layout
 				if( get_row_layout() == 'category_section' ): ?>
-				<article class="<?php echo get_sub_field('category') ?>">
-					<img class="bg-image" src="<?php echo get_sub_field('background_image'); ?>" alt="">
-					<div class="container">
+
+					<article class="<?php echo get_sub_field('category') ?>">
+						<img class="bg-image" src="<?php echo get_sub_field('background_image'); ?>" alt="">
+						<div class="container">
+							<div class="row">
+									<div class="<?php if ( get_sub_field('image_display') == 'left' ){ echo 'col-sm-12 col-lg-5'; } else { echo 'col-sm-6 col-lg-5 offset-lg-1 flex-last'; } ?>">
+										<img class="page__lead__img" src="<?php echo get_sub_field('image'); ?>" alt="">									
+										<?php if ( get_sub_field('image_display') == 'left' ){ ?>
+											<img class="accent-left" src="./wp-content/themes/voyager-theme/images/accent-left.png" alt="">
+										<?php } else { ?>
+											<img class="accent-right" src="./wp-content/themes/voyager-theme/images/accent-right.png" alt="">
+										<?php } ?>
+									</div>
+									<div class="col-sm-6 <?php if ( get_sub_field('image_display') == 'left' ){ echo 'offset-lg-1'; } ?>">
+										<header>
+											<h4 class="subtitle">
+												<?php echo get_sub_field('subheader'); ?>
+											</h4>
+											<h1 class="title">
+												<?php echo get_sub_field('title') ?>
+											</h1>
+										</header>
+										<hr>
+										<p class="topic">
+											<?php echo get_sub_field('subtitle'); ?>
+										</p>
+										<p>
+											<?php echo get_sub_field('content'); ?>
+										</p>
+										<?php if( get_sub_field('button') ): ?>
+											<a href="#" class="btn btn-primary"><?php echo get_sub_field('button'); ?></a>
+										<?php endif; ?>
+									</div>
+							</div>
+						</div>
+					</article>
+					<div class="container section__posts <?php echo get_sub_field('category') ?>">
+						<?php $section_posts =  new WP_Query ( array(
+							'cat' => get_sub_field('posts'),
+							'posts_per_page' => 4
+							)
+						); ?>
+						<h2 class="section__posts__title">
+							<?php echo get_sub_field('posts_title') ?>
+							<br>
+							<hr>
+						</h2>
 						<div class="row">
-								<div class="<?php if ( get_sub_field('image_display') == 'left' ){ echo 'col-sm-12 col-lg-5'; } else { echo 'col-sm-6 col-lg-5 offset-lg-1 flex-last'; } ?>">
-									<img class="page__lead__img" src="<?php echo get_sub_field('image'); ?>" alt="">									
-									<?php if ( get_sub_field('image_display') == 'left' ){ ?>
-										<img class="accent-left" src="./wp-content/themes/voyager-theme/images/accent-left.png" alt="">
-									<?php } else { ?>
-										<img class="accent-right" src="./wp-content/themes/voyager-theme/images/accent-right.png" alt="">
-									<?php } ?>
-								</div>
-								<div class="col-sm-6 <?php if ( get_sub_field('image_display') == 'left' ){ echo 'offset-lg-1'; } ?>">
-									<header>
-										<h4 class="subtitle">
-											<?php echo get_sub_field('subheader'); ?>
-										</h4>
-										<h1 class="title">
-											<?php echo get_sub_field('title') ?>
-										</h1>
-									</header>
-									<hr>
-									<p class="topic">
-										<?php echo get_sub_field('subtitle'); ?>
-									</p>
-									<p>
-										<?php echo get_sub_field('content'); ?>
-									</p>
-									<?php if( get_sub_field('button') ): ?>
-										<a href="#" class="btn btn-primary"><?php echo get_sub_field('button'); ?></a>
-									<?php endif; ?>
-								</div>
+							<?php if ( $section_posts->have_posts() ) : ?>
+								<?php while ( $section_posts->have_posts() ) : $section_posts->the_post(); ?>
+									<div class="col-lg-3 col-sm-6">								
+										<article class="section__post">								
+											<a href="" class="thumbnail" title="<?php the_title_attribute(); ?>" style="background-image:url('<?php echo get_the_post_thumbnail_url() ?>');"></a>								
+											<h3 class="title">
+												<a href="<?php the_permalink(); ?>">
+													<?php the_title(); ?>
+												</a>
+											</h3>
+										</article>
+									</div>
+								<?php endwhile; ?>
+								<?php wp_reset_postdata(); ?>
+							<?php endif; ?>
 						</div>
 					</div>
-				</article>
-				<div class="container section__posts <?php echo get_sub_field('category') ?>">
-					<?php $section_posts =  new WP_Query ( array(
-						'cat' => get_sub_field('posts'),
-						'posts_per_page' => 4
-						)
-					); ?>
-					<h2 class="section__posts__title">
-						<?php echo get_sub_field('posts_title') ?>
-						<br>
-						<hr>
-					</h2>
-					<div class="row">
-						<?php if ( $section_posts->have_posts() ) : ?>
-							<?php while ( $section_posts->have_posts() ) : $section_posts->the_post(); ?>
-								<div class="col-lg-3 col-sm-6 col-xs-12">								
-									<article class="section__post">								
-										<a href="" class="thumbnail" title="<?php the_title_attribute(); ?>" style="background-image:url('<?php echo get_the_post_thumbnail_url() ?>');"></a>								
-										<h3 class="title">
-											<a href="<?php the_permalink(); ?>">
-												<?php the_title(); ?>
-											</a>
-										</h3>
-									</article>
-								</div>
-							<?php endwhile; ?>
-							<?php wp_reset_postdata(); ?>
-						<?php endif; ?>
-					</div>
-				</div>
+			
 
-			<?php endif;
+			<?php // Quote Layout
+				elseif (get_row_layout() == 'quote') : ?>
+				
+					<div class="quote container <?php echo get_sub_field('category_color') ?>">
+						<div class="row">
+							<div class="col-10 offset-1">
+								<h5 class="quote__bg">
+									success
+								</h5>
+								<div class="row">
+									<div class="col-sm-1 quote__icon">
+										<?php if ( get_sub_field('category_color') == 'adopt' ) : ?>
+											<img class="" src="./wp-content/themes/voyager-theme/images/quote-adopt.png" alt="">
+										<?php elseif ( get_sub_field('category_color') == 'programs') : ?>
+											<img class="" src="./wp-content/themes/voyager-theme/images/quote-programs.png" alt="">
+										<?php endif; ?>
+									</div>
+									<div class="col-sm-11">									
+										<h4 class="quote__text">
+											<?php echo get_sub_field('quote_text') ?>
+										</h4>							
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+			<?php endif; ?>
+
+			<?php
 			endwhile;
 			endif; 
 			?>
---
+++ single-adopt.php
@@ -0,0 +1,157 @@
+<?php
+/*
+ * Template Name: Pet
+ * Template Post Type: post
+ *
+ * @package Voyager_Theme
+ */
+ 
+ get_header();  ?>
+
+	
+	<div class="page__header adopt" style="<?php if( get_field('field_name') ): ?>background-image: url(<?php the_field('header_image'); endif; ?>);">
+		<h1 class="page__title">
+			<?php the_title(); ?>
+			<br>
+			<hr>	
+		</h1>
+	</div>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main container" role="main">
+			<section>
+				<div class="row">
+					<div class="col-sm-12">
+						<p class="return">
+							<i class="fa fa-angle-left"></i>
+							<i class="fa fa-angle-left"></i>
+							<i class="fa fa-angle-left"></i>
+							return to
+							<a href="/">available animals</a>
+						</p>
+					</div>
+				</div>
+			</section>
+			<section class="page-content">
+				<div class="row">
+					<div class="col-6">						
+						<img class="page__lead" src="<?php the_field('pet_image'); ?>" alt="">
+						<img class="accent-left" src="<?php echo get_home_url(); ?>/wp-content/themes/voyager-theme/images/accent-left.png" alt="" class="accent-left">
+					</div>
+					<article id="post-<?php the_ID(); ?>" <?php post_class('adopt col-5 offset-1'); ?>>
+						<header>
+							<h4 class="subtitle">
+								<?php the_field('content_subheader'); ?>
+							</h4>
+							<h1 class="title">
+								<?php the_field('content_title') ?>
+							</h1>
+							<hr>
+							<p class="topic">
+								<?php the_field('content_subtitle'); ?>
+							</p>
+						</header>
+
+						<div class="entry-content">
+							<?php
+							the_field('content_paragraph');
+
+							wp_link_pages( array(
+								'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'voyager-theme' ),
+								'after'  => '</div>',
+								) );
+							?>
+						</div>
+					</article>
+				</div>
+				<div class="row pet__details">
+					<div class="col-2 pl-0 pet__details--age">
+						<?php the_field('pet_age'); ?>
+						<h5>Age</h5>
+					</div>
+					<div class="col-2 pl-0 pet__details--size">
+						<?php the_field('pet_size'); ?>
+						<h5>Size</h5>
+					</div>
+					<div class="col-2 pl-0 pet__details--av">
+						<?php the_field('pet_availability'); ?>
+						<h5>Available</h5>
+					</div>
+					<div class="col-5 offset-1">
+						<a href="#" class="btn btn-primary">
+							adopt me
+						</a>
+					</div>
+				</div>
+				<div class="container section__posts adopt">
+						<?php $section_posts =  new WP_Query ( array(
+							'cat' => 'adoption',
+							'posts_per_page' => 4
+							)
+						); ?>
+						<h2 class="section__posts__title">
+							available pets
+							<br>
+							<hr>
+						</h2>
+						<div class="row">
+							<?php if ( $section_posts->have_posts() ) : ?>
+								<?php while ( $section_posts->have_posts() ) : $section_posts->the_post(); ?>
+									<div class="col-lg-3 col-sm-6">								
+										<article class="section__post">								
+											<a href="" class="thumbnail" title="<?php the_title_attribute(); ?>" style="background-image:url('<?php echo get_the_post_thumbnail_url() ?>');"></a>								
+											<h3 class="title">
+												<a href="<?php the_permalink(); ?>">
+													<?php the_title(); ?>
+												</a>
+											</h3>
+										</article>
+									</div>
+								<?php endwhile; ?>
+								<?php wp_reset_postdata(); ?>
+							<?php endif; ?>
+						</div>
+					</div>
+
+					<?php
+			if( have_rows('flexible_content') ):
+				while ( have_rows('flexible_content') ) : the_row(); 
+					if (get_row_layout() == 'quote') : ?>
+
+					<div class="quote container <?php echo get_sub_field('category_color') ?>">
+								<div class="row">
+									<div class="col-10 offset-1">
+										<h5 class="quote__bg">
+											success
+										</h5>
+										<div class="row">
+											<div class="col-sm-1 quote__icon">
+												<?php if ( get_sub_field('category_color') == 'adopt' ) : ?>
+													<img class="" src="<?php get_home_url() ?>/wp-content/themes/voyager-theme/images/quote-adopt.png" alt="">
+												<?php elseif ( get_sub_field('category_color') == 'programs') : ?>
+													<img class="" src="<?php get_home_url() ?>/wp-content/themes/voyager-theme/images/quote-programs.png" alt="">
+												<?php endif; ?>
+											</div>
+											<div class="col-sm-11">									
+												<h4 class="quote__text">
+													<?php echo get_sub_field('quote_text') ?>
+												</h4>							
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+
+			<?php endif; ?>
+
+			<?php
+			endwhile;
+			endif; 
+			?>
+
+			</section>			
+		</main>
+	</div>
+
+<?php
+get_footer();
