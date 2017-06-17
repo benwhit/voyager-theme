e976d76457dbc40481e92a124506f2ac4b569ccd
ben <bwhitlock@cyberbility.com>
2017-06-16 22:41:30 -0400

    cleaned up acf fields

--- partials/category-section.php
+++ partials/category-section.php
@@ -4,12 +4,12 @@
 	<div class="container">
 		<div class="row">
 				<div class="<?php if ( get_sub_field('image_display') == 'left' ){ echo 'col-sm-12 col-lg-5'; } else { echo 'col-sm-6 col-lg-5 offset-lg-1 flex-last'; } ?>">
-					<img class="page__lead__img" src="<?php echo get_sub_field('image'); ?>" alt="">									
 					<?php if ( get_sub_field('image_display') == 'left' ){ ?>
 						<img class="accent-left" src="./wp-content/themes/voyager-theme/images/accent-left.png" alt="">
 					<?php } else { ?>
 						<img class="accent-right" src="./wp-content/themes/voyager-theme/images/accent-right.png" alt="">
 					<?php } ?>
+					<img class="page__lead__img" src="<?php echo get_sub_field('image'); ?>" alt="">									
 				</div>
 				<div class="col-sm-6 <?php if ( get_sub_field('image_display') == 'left' ){ echo 'offset-lg-1'; } ?>">
 					<header>
--- partials/posts-carousel.php
+++ partials/posts-carousel.php
@@ -1,24 +1,40 @@
 <!-- Posts Carousel Block -->
-<div class="container section__posts <?php echo get_sub_field('category_color') ?>">
-	<?php $section_posts =  new WP_Query ( array(
+<?php
 
-		// Create WP Query from selected category
-		'cat' => get_sub_field('posts'),
-			'posts_per_page' => 4
-			)
-		); ?>
-		
+global $post_count;
+if ( empty($post_count) ) { $post_count = 0; }
+
+if (is_page('adopt')) {
+	$section_posts =  new WP_Query ( array(
+		'posts_per_page' 	=> 8,
+		'cat' 						=> get_sub_field('posts'),
+		'offset' 					=> $post_count
+	));
+} else {
+	$section_posts =  new WP_Query ( array(
+		'posts_per_page' 	=> 8,
+		'cat' 						=> get_sub_field('posts')
+	)); 
+} 
+
+?>
+
+<div class="container section__posts <?php echo get_sub_field('category_color') ?>">		
 		<!-- Posts Carousel Title -->
-		<h2 class="section__posts__title">
-			<?php echo get_sub_field('posts_title') ?>
-			<br>
-			<hr>
-		</h2>
+		<?php if (get_sub_field('posts_title')) : ?>
+			<h2 class="section__posts__title">
+				<?php echo get_sub_field('posts_title') ?>
+				<br>
+				<hr>
+			</h2>
+		<?php endif; ?>
 
 		<!-- Posts Carousel Loop -->
 		<div class="row">
 			<?php if ( $section_posts->have_posts() ) : ?>
 				<?php while ( $section_posts->have_posts() ) : $section_posts->the_post(); ?>
+
+
 					<div class="col-lg-3 col-sm-6">								
 						<article class="section__post">								
 							<a href="" class="thumbnail" title="<?php the_title_attribute(); ?>" style="background-image:url('<?php echo get_the_post_thumbnail_url() ?>');"></a>								
@@ -29,6 +45,7 @@
 							</h3>
 						</article>
 					</div>
+					<?php $post_count++; ?>
 				<?php endwhile; ?>
 				<?php wp_reset_postdata(); ?>
 			<?php endif; ?>
