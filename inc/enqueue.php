4a72674bb1e016398bd2e49ad17a8fef963663db
ben <bwhitlock@cyberbility.com>
2017-06-22 11:29:31 -0400

    performnace functions

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -6,13 +6,13 @@ function voyager_theme_scripts() {
 
 	// Styles
 
-	wp_enqueue_style( 'voyager-theme-style', get_template_directory_uri() . '/css/styles.css' );
+	wp_enqueue_style( 'voyager-theme-style', get_template_directory_uri() . '/css/styles.css', array(), null );
 
 	// Scripts
 	
-	wp_enqueue_script( 'tether', '//cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js', array(), '20151215', true );
+	wp_enqueue_script( 'tether', '//cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js', array(), null, true );
 
-	wp_enqueue_script( 'voyager', get_template_directory_uri() . '/js/script.js', array(), '20151215', true );
+	wp_enqueue_script( 'voyager', get_template_directory_uri() . '/js/script.js', array(), null, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
