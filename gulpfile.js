f22bb498e00c854dfce6eb882503750b49cca0f6
ben <ben@voyagermark.com>
2018-04-15 14:32:06 -0400

    updated gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,31 @@ var plumber = require('gulp-plumber');
 var deporder = require('gulp-deporder');
 var cleanCSS = require('gulp-clean-css');
 var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 var imagemin = require('gulp-imagemin');
+var cssBase64 = require('gulp-css-base64');
+var gcmq = require('gulp-group-css-media-queries');
+var autoprefixer = require('gulp-autoprefixer');
+var notify = require("gulp-notify");
+var rename = require("gulp-rename");
+var sassLint = require('gulp-sass-lint');
 
 // BrowserSync
 gulp.task('browser-sync', function() {
-	browserSync.init([
-		"css/*.css",
-		"js/*.js",
-		"*.html",
-		"*.php",
-		"**/*.php"], {
+	browserSync.init({
+
+    // Project URL.
 		proxy: "local.wordpress.test", // change to local server url
+
+    // `true` Automatically open the browser with BrowserSync live server.
+    // `false` Stop the browser from automatically opening.
+    open: true,
+
+    // Inject CSS changes.
+    // Commnet it to reload browser for every CSS change.
+    injectChanges: true,
+
+    // Use a specific port (instead of the one auto-detected by Browsersync).
 		port: 3000
 	});
 });
@@ -26,9 +40,28 @@ gulp.task('browser-sync', function() {
 gulp.task('sass', function () {
 	gulp.src('sass/voyager.scss')
 	.pipe(plumber())
-	.pipe(sass({includePaths: ['scss']}))
+	.pipe( sass( {
+			includePaths: ['scss'],
+      errLogToConsole: true,
+      outputStyle: 'compact',
+      // outputStyle: 'compressed',
+      // outputStyle: 'nested',
+      // outputStyle: 'expanded',
+      precision: 10
+   	} ) )
+  .on('error', console.error.bind(console))
+	.pipe(gcmq())
+  .pipe(cssBase64())
+  .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie 10'],
+      cascade: false
+  }))
+  .pipe(sassLint())
+	.pipe(gulp.dest('css'))
+  .pipe( rename( { suffix: '.min' } ) )
 	.pipe(cleanCSS())
 	.pipe(gulp.dest('css'))
+	.pipe( notify( { message: 'TASK: "styles" Completed! 💯', onLast: true } ) )
 });
 
 // Javascript
@@ -37,8 +70,10 @@ gulp.task('js', function(){
 	.pipe(plumber())
 	.pipe(deporder())
 	.pipe(concat('script.js'))
+  .pipe( rename( { suffix: '.min' } ) )
 	.pipe(uglify())
 	.pipe(gulp.dest('js/'))
+	.pipe( notify( { message: 'TASK: "js" Completed! 🚀', onLast: true } ) )
 });
 
 // Imagemin
@@ -51,8 +86,8 @@ gulp.task('imagemin', () =>
 
 // Gulp Default Task
 gulp.task('default', ['sass', 'js', 'imagemin', 'browser-sync'], function () {
-	gulp.watch("sass/*.scss", ['sass']);
-	gulp.watch("sass/**/*.scss", ['sass']);
+	gulp.watch( './**/*.php', reload );
+	gulp.watch("./**/*.scss", ['sass']);
 	gulp.watch("js/src/*.js", ['js']);
 	gulp.watch('./img/**', ['imagemin'])
 });
--- inc/enqueue.php
+++ inc/enqueue.php
@@ -4,8 +4,14 @@
  */
 function voyager_theme_scripts() {
 
-	wp_enqueue_style( 'voyager-style', get_template_directory_uri() . '/css/voyager.css');
-	wp_enqueue_script( 'voyager-script', get_template_directory_uri() . '/js/script.js', array('jquery'), null, true );
+	// Development
+	// wp_enqueue_style( 'voyager-style', get_template_directory_uri() . '/css/voyager.css');
+	// wp_enqueue_script( 'voyager-script', get_template_directory_uri() . '/js/script.js', array('jquery'), null, true );
+
+	// Production
+	wp_enqueue_style( 'voyager-style', get_template_directory_uri() . '/css/voyager.min.css');
+	wp_enqueue_script( 'voyager-script', get_template_directory_uri() . '/js/script.min.js', array('jquery'), null, true );
+
 	wp_enqueue_script( 'popper', 'https://unpkg.com/popper.js@1.12.9/dist/umd/popper.js', array(), null, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
