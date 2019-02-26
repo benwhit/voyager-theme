23c1f5ad794c6857dbd4b19ae41bc413f218dcd7
ben <ben@voyagermark.com>
2019-02-26 09:12:46 -0500

    snr underscores

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -2,15 +2,15 @@
 /**
  * Functions which enhance the theme by hooking into WordPress
  *
- * @package _s
+ * @package Voyager_Starter
  */
 
 /**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
-function _s_pingback_header() {
+function voyager_starter_pingback_header() {
 	if ( is_singular() && pings_open() ) {
 		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
 	}
 }
-add_action( 'wp_head', '_s_pingback_header' );
+add_action( 'wp_head', 'voyager_starter_pingback_header' );
